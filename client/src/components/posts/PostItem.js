fb960f18e2fb364d2f5a4c5b27eedfd350ada567
Michael Aldrich Serrano <mykeserrano@Michaels-iMac.local>
2019-10-21 18:29:21 -0400

    add showaction in postItem

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -51,7 +51,7 @@ const PostItem = ({
           </Link>
           {!auth.loading && user === auth.user._id && (
             <button
-              onClick={e => deletePost(_id)}
+              onClick={() => deletePost(_id)}
               type='button'
               className='btn btn-danger'
             >
@@ -73,7 +73,8 @@ PostItem.propTypes = {
   auth: PropTypes.object.isRequired,
   addLike: PropTypes.func.isRequired,
   removeLike: PropTypes.func.isRequired,
-  deletePost: PropTypes.func.isRequired
+  deletePost: PropTypes.func.isRequired,
+  showActions: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
