7fd59de9a85ca9565beef76a29c6350b658240f8
Michael Aldrich Serrano <mykeserrano@Michaels-iMac.local>
2019-10-21 18:38:38 -0400

    defined moment

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+import moment from 'moment';
 
 const ProfileExperience = ({
   experience: { company, title, location, current, to, from, description }
